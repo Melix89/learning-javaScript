665314b4171944228d6479ece934749ebe6ab894
Melix89 <melissa.onori@gmail.com>
2013-03-06 23:03:16 +0100

    Update exercise03.js

--- 2013-03-04/exercise03.js
+++ 2013-03-04/exercise03.js
@@ -1,24 +1,22 @@
 
 
-
 console.log("Exercise02");
 
 var mat = "";
 
 for(var i=1; i<=10; i++){
         for(var j=1; j<10; j++){
-                if(i=j){
+                if(i===j)
 			mat += 1 + "," + "\t";
-		}else
-			mat += 0 + "," + "\t";
-        	}
+		else
+			mat += 0 + "," + "\t";	
 	}
 
-	if(j=j){
+	if(i===j)
 	        mat += 1 + "\n";
-	}else{
+	else
 		mat += 0 + "\n";
-	}
+	
 }
 
 console.log(mat);
